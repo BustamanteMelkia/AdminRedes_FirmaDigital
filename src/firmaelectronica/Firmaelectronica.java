93ff859bfd26f507509dbfc298af930783e24764
melkia bustamante <bonfil99melkia@gmail.com>
2021-04-26 15:36:19 -0500

    View added

---
+++ src/firmaelectronica/Firmaelectronica.java
@@ -0,0 +1,9 @@
+package firmaelectronica;
+public class Firmaelectronica {
+    public static void main(String[] args) {
+        /*Ejecucion de la vista principal*/
+        principal ibj = new principal();
+        ibj.setVisible(true);
+    }
+    
+}
